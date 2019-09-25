f902eaccb3aff7ba45d26b55917c7c685473bc90
Winant Pablo <pablo.winant@gmail.com>
2019-09-25 21:20:24 +0200

    CI: fixed import

--- dolark/__init__.py
+++ dolark/__init__.py
@@ -5,7 +5,8 @@ print("DolARK loading:  ", end="\r")
 symbs = ["|", "\\", "-", "/"]
 i = 0
 
-while True:
+I = 10
+for i in range(I):
     time.sleep(0.5)
     i = (i+1) % 4
     print(f"DolARK loading: {symbs[i]}", end="\r")
