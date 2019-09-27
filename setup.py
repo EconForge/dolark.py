8e5f69a3b7cd3d7a88872e57f78532bf39aa1964
Pablo Winant <pablo.winant@gmail.com>
2019-09-27 18:32:44 +0200

    Mybinder: added package to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 setup(name='dolark',
       version='0.0.1',
+      packages=['dolark'],
       description='What is not clear about mixing together Dolo and ARK?',
       author='Pablo Winant',
       author_email='pablo.winant@gmail.com',
