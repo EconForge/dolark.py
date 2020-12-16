Fixed BF 2017 SS calculation

experiments/dev_hmodel.py
@@ -17,9 +17,14 @@ print(hmodel1.name)
 
 hmodel2 = HModel("ayiagari_betadist.yaml")
 print(hmodel2.name)
+
+hmodel3 = HModel("bfs_2017.yaml")
+print(hmodel3.name)
 # %%
 eq1 = find_steady_state(hmodel1)
 eq2 = find_steady_state(hmodel2)
+# %%
+eq3 = find_steady_state(hmodel3)
 # %%
 # Distribution of assets for each idiosyncratic state
 from matplotlib import pyplot as plt
@@ -38,9 +43,15 @@ for i, (w, eq) in enumerate(eq2):
 plt.legend()
 # %%
 # Wealth distribution
+# Aiyagari
 s = eq1.dr.endo_grid.nodes
 plt.plot(s, eq1.μ.sum(axis=0), "-o")
 # %%
+# BF
+s = eq3.dr.endo_grid.nodes
+plt.plot(s, eq3.μ)
+# %%
+# Aiyagari H beta
 bins = []
 for w, eq in eq2:
     bins.append(w * sum(s.ravel() * eq.μ.sum(axis=0)))
@@ -52,11 +63,14 @@ plt.plot([e[1]["β"] for e in dist], bins, "-o")
 plt.xlabel("β")
 # %%
 # Steady-state equilibrium: graphic resolution
+from tqdm import tqdm
+# %%
+# Aiyagari
 dr0 = hmodel1.get_starting_rule()
 m0 = hmodel1.calibration["exogenous"]
 
 kvec = np.linspace(30, 50, 20)
-hmodel1.model.set_calibration(**kwargs)
+hmodel1.model.set_calibration({})
 eqs = [
     equilibrium(hmodel1, m0, np.array([k]), dr0=dr0, return_equilibrium=False)
     for k in tqdm(kvec)
@@ -68,8 +82,25 @@ from matplotlib import pyplot as plt
 
 plt.plot(kvec, kvec - eqs, color="black")
 # %%
-from tqdm import tqdm
+# BF
+dr0 = hmodel3.get_starting_rule()
+m0 = hmodel3.calibration["exogenous"]
 
+kvec = np.linspace(0.1, 10, 10)
+hmodel3.model.set_calibration({})
+eqs = [
+    equilibrium(hmodel3, m0, np.array([k]), dr0=dr0, return_equilibrium=False)
+    for k in tqdm(kvec)
+]
+
+eqs = [e[0] for e in eqs]
+# %%
+from matplotlib import pyplot as plt
+
+plt.plot(kvec, kvec - eqs, color="black")
+
+# %%
+# Aiyagari H beta
 dr0 = hmodel2.get_starting_rule()
 m0 = hmodel2.calibration["exogenous"]
 
@@ -91,21 +122,3 @@ for eq in eqs:
 plt.plot(kvec, kvec - sum(eqs, 0) * 0.5, linestyle="--", color="black")
 plt.plot(kvec, kvec, color="black")
 plt.grid()
-#%%
-
-from dolark.perturbation import perturb
-from dolo import groot
-from dolark import HModel
-from dolark.equilibrium import find_steady_state
-
-groot("examples")
-
-hmodel1 = HModel("ayiagari.yaml")
-print(hmodel1.name)
-eq = find_steady_state(hmodel1)
-perteq = perturb(hmodel1, eq1)
-# %%
-hmodel3 = HModel("bfs_2017.yaml")
-print(hmodel3.name)
-
-eq3 = find_steady_state(hmodel3)